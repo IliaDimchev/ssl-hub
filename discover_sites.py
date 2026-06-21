387c70521b277b69b4cd281bbffcf3db02ec1bfa
ilkoTheTiger <ilkothetiger@gmail.com>
2026-06-22 01:11:24 +0300

    discover-sites.py refactored to add sites instead of just printing it

--- discover_sites.py
+++ discover_sites.py
@@ -1,6 +1,10 @@
+import sqlite3
 from pathlib import Path
+from config import DB
 
 HOME = Path("/home/ilirbktk")
+
+conn = sqlite3.connect(DB)
 
 for item in HOME.iterdir():
 
@@ -9,9 +13,58 @@ for item in HOME.iterdir():
 
     wp = item / "wp-config.php"
 
-    if wp.exists():
+    if not wp.exists():
+        continue
 
-        print(
-            item.name,
-            item.absolute()
+    domain = item.name
+    webroot = str(item)
+
+    cursor = conn.execute(
+        """
+        SELECT id
+        FROM domains
+        WHERE domain=?
+        """,
+        (domain,)
+    )
+
+    row = cursor.fetchone()
+
+    if row:
+
+        conn.execute(
+            """
+            UPDATE domains
+            SET
+                wordpress=1,
+                webroot=?
+            WHERE domain=?
+            """,
+            (
+                webroot,
+                domain
+            )
         )
+
+    else:
+
+        conn.execute(
+            """
+            INSERT INTO domains
+            (
+                domain,
+                webroot,
+                wordpress
+            )
+            VALUES
+            (?, ?, 1)
+            """,
+            (
+                domain,
+                webroot
+            )
+        )
+
+conn.commit()
+
+print("Sites discovered")
