1e0a965a61390241c97bbb5b8a04fa1ec9ff50fd
Ilirium <ilkothetiger@gmail.com>
2026-06-21 16:43:34 -0400

    app.py added after modification

--- app.py
+++ app.py
@@ -1,50 +1,13 @@
-from flask import Flask
-from engine.domains import load_domains
-from engine.certs import days_left
-
-app = Flask(__name__)
+import os
+import sys
 
 
-@app.route("/")
-def dashboard():
-
-    domains = load_domains()
-
-    rows = []
-
-    for item in domains["domains"]:
-
-        try:
-            days = days_left(
-                item["domain"]
-            )
-
-        except Exception:
-
-            days = "?"
-
-        rows.append({
-            "domain": item["domain"],
-            "days": days
-        })
-
-    html = "<h1>SSL Hub</h1>"
-
-    html += "<table border='1'>"
-
-    for row in rows:
-
-        html += (
-            f"<tr>"
-            f"<td>{row['domain']}</td>"
-            f"<td>{row['days']}</td>"
-            f"</tr>"
-        )
-
-    html += "</table>"
-
-    return html
+sys.path.insert(0, os.path.dirname(__file__))
 
 
-if __name__ == "__main__":
-    app.run()
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
